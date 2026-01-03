a7bd5c5ce94717514af393cc51ab217115c925e1
prahlad makwana <prahalad13042006@gmail.com>
2026-01-03 20:54:24 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,18 @@
+name="prahlad makwana"
+address="ratanpar,umrala"
+city="bhavnagar"
+pincode="364330"
+age="19"
+weight="43"
+gender="male"
+print(name)
+print(address)
+print("city")
+print("pincode")
+print("age")
+print("weight")
+print("gender")
+print("name",name,"address",address,"city",city,"pincode",pincode,"age",age,"weight",weight,"gender",gender)
+print(name[0:3])
+print(address[4:10])
+print(age[0:2]+pincode[0:6])
