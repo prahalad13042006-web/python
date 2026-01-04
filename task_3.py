b4f3e2b590142f189b2e38339205514d40d9c70a
prahlad makwana <prahalad13042006@gmail.com>
2026-01-04 21:35:45 +0530

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,9 @@
+name=input("your name is..")
+age=input("your age is..")
+gender=input("male or female..")
+city=input("your city name is..")
+semester=input("your semester is..")
+address=input("your address is..")
+
+
+print("your name is..",name,"your age is..",age,"your gender is",gender,"your city is..",city,"your semester is",semester,"your address is..",address)
