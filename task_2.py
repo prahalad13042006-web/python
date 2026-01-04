2a587a0258a88b5868313db81549349a27b46b7c
prahlad makwana <prahalad13042006@gmail.com>
2026-01-04 21:13:43 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,9 @@
+collagename=["shree swaminarayan collage of computer science"]
+branch=["bhavnagar"]
+semester=["6"]
+hobbies=["playing cricket,games,etc.."]
+print(collagename)
+print(branch)
+print(semester)
+print(hobbies)
+print("collage name is",collagename,"branch name is",branch,"semester is",semester,"hobbies is",hobbies)
