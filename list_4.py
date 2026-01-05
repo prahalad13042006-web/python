69e3f97c68d246da8890893f7a94f3bc083eca07
prahlad makwana <prahalad13042006@gmail.com>
2026-01-05 21:21:26 +0530

    Add files via upload

---
+++ list_4.py
@@ -0,0 +1,24 @@
+fruits= [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Mango",
+    "Grapes",
+    "Pineapple",
+    "Strawberry",
+    "mango",
+    "mango",
+    "Pear"
+]
+print("fruits")  
+
+print(fruits.count("mango"))
+print(fruits.count("pear"))
+fruits2=fruits
+print("fruits2")
+print(fruits[0:5])
+print(fruits[7])
+print(fruits.remove("mango"))
+print(fruits.extend("pear"))
+
+
