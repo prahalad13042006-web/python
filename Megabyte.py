160665eea50d6528fadcfe12a1b9460391d9d101
prahlad makwana <prahalad13042006@gmail.com>
2026-01-12 23:30:40 +0530

    Create Megabyte.py

---
+++ Megabyte.py
@@ -0,0 +1,9 @@
+
+#Write a program to convert kilobytes into megabytes.
+
+
+
+kilobytes = float(input("Enter value in kilobytes: "))
+megabytes = kilobytes / 1024
+
+print(f"{kilobytes} KB is equal to {megabytes} MB")
